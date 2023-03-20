9bf2841d34d65dd2c1636fc790fb55648036eabc
William Parsons <avolitty@gmail.com>
2023-03-19 20:11:00 -0700

    Deleting redundant integral types in header.

--- src/avolitty-hasher.h
+++ src/avolitty-hasher.h
@@ -1,27 +1,27 @@
 #ifndef AVOLITTY_HASHER_H
 #define AVOLITTY_HASHER_H
 
-void AvolittyHasherA(unsigned short int * a, unsigned short int * b, unsigned short int * c, unsigned short int d, unsigned char * e);
+void AvolittyHasherA(unsigned short * a, unsigned short * b, unsigned short * c, unsigned short d, unsigned char * e);
 
-void AvolittyHasherB(unsigned short int * a, unsigned short int * b, unsigned short int * c, unsigned short int d, unsigned char * e);
+void AvolittyHasherB(unsigned short * a, unsigned short * b, unsigned short * c, unsigned short d, unsigned char * e);
 
-void AvolittyHasherC(unsigned short int * a, unsigned short int * b, unsigned short int * c, unsigned short int d, unsigned char * e);
+void AvolittyHasherC(unsigned short * a, unsigned short * b, unsigned short * c, unsigned short d, unsigned char * e);
 
-void AvolittyHasherD(unsigned short int * a, unsigned short int * b, unsigned short int * c, unsigned short int d, unsigned char * e);
+void AvolittyHasherD(unsigned short * a, unsigned short * b, unsigned short * c, unsigned short d, unsigned char * e);
 
-void AvolittyHasherE(unsigned short int * a, unsigned short int * b, unsigned short int * c, unsigned short int d, unsigned char * e);
+void AvolittyHasherE(unsigned short * a, unsigned short * b, unsigned short * c, unsigned short d, unsigned char * e);
 
-void AvolittyHasherF(unsigned short int * a, unsigned short int * b, unsigned short int * c, unsigned short int d, unsigned char * e);
+void AvolittyHasherF(unsigned short * a, unsigned short * b, unsigned short * c, unsigned short d, unsigned char * e);
 
-void AvolittyHasherG(unsigned short int * a, unsigned short int * b, unsigned short int * c, unsigned short int d, unsigned char * e);
+void AvolittyHasherG(unsigned short * a, unsigned short * b, unsigned short * c, unsigned short d, unsigned char * e);
 
-void AvolittyHasherH(unsigned short int * a, unsigned short int * b, unsigned short int * c, unsigned short int d, unsigned char * e);
+void AvolittyHasherH(unsigned short * a, unsigned short * b, unsigned short * c, unsigned short d, unsigned char * e);
 
-void AvolittyHasherI(unsigned short int * a, unsigned short int * b, unsigned short int * c, unsigned short int d, unsigned char * e);
+void AvolittyHasherI(unsigned short * a, unsigned short * b, unsigned short * c, unsigned short d, unsigned char * e);
 
-void AvolittyHasherJ(unsigned short int * a, unsigned short int * b, unsigned short int * c, unsigned short int d, unsigned char * e);
+void AvolittyHasherJ(unsigned short * a, unsigned short * b, unsigned short * c, unsigned short d, unsigned char * e);
 
-void AvolittyHasherK(unsigned short int * a, unsigned short int * b, unsigned short int * c, unsigned short int d, unsigned char * e);
+void AvolittyHasherK(unsigned short * a, unsigned short * b, unsigned short * c, unsigned short d, unsigned char * e);
 
 void AvolittyHasherL(unsigned short * a, unsigned short b, unsigned short c, unsigned char * d);
 
